10291b848e10eba60c2fc0b71bd294c58535707d
kknaks <benesia93@naver.com>
2024-07-16 09:45:34 +0900

    .'

--- app/src/main/java/bitcamp/project3/command/DayOverCommand.java
+++ app/src/main/java/bitcamp/project3/command/DayOverCommand.java
@@ -22,35 +22,39 @@ public class DayOverCommand implements Command {
 
   @Override
   public void execute(String menuName) {
-    System.out.println("------영업종료--------");
-    System.out.println("------정산내역--------");
-    //정산내역 정리하기
     List<RentInfo> rentGuest = new ArrayList<>();
     List<RentInfo> returnTrue = new ArrayList<>();
     List<RentInfo> returnFalse = new ArrayList<>();
 
     for (RentInfo rentInfo : rentList) {
+      BookInfo book = getBook(rentInfo.getBookName());
       if (rentInfo.getRentStartDate().equals(storeInfos.getDate())) {
         rentGuest.add(rentInfo);
       }
       if (rentInfo.getRentEndDate().equals(storeInfos.getDate())) {
         if (rentInfo.isBookReturn()) {
           returnTrue.add(rentInfo);
+          book.setStock(book.getStock() + 1);
         } else {
           returnFalse.add(rentInfo);
+          book.setStock(book.getStock() - 1);
         }
       }
     }
+    System.out.println("------영업종료--------");
+    //정산내역 정리하기
     Prompt.input("계속하려면 엔터를 누르세요.");
-    System.out.printf("대출권수 : %d", rentGuest.size());
-    System.out.printf("반납 받은 책 : %d", returnTrue.size());
-    // 명성 몇 +? 피로도 몇 +? 자금 몇 +?
+    System.out.println("------정산내역--------");
+    System.out.printf("대출권수 : %d 권\n", rentGuest.size());
 
-    //분실한 책 출력
-    //대출 목록에서 endDate도래 한것들을 선택 반납여부 확인
-    //반납하면 +1, 분실하면 -1
-    //도래된것은 rentlist에서 제거
-    //북리스트에서 분실한 책 수 만큼 빼기
+    System.out.printf("반납 받은 책 : %d 권\n", returnTrue.size());
+
+    for (RentInfo rentInfo : returnTrue) {
+      System.out.printf("[%s]가 [%s]를 반납했습니다.\n", rentInfo.getGuestType(), rentInfo.getBookName());
+    }
+    for (RentInfo rentInfo : returnFalse) {
+      System.out.printf("[%s]가 [%s]를 반납 안했습니다.\n", rentInfo.getGuestType(), rentInfo.getBookName());
+    }
     System.out.println("-------------------------");
     storeInfos.setDate(storeInfos.getDate().plusDays(1));
   }
